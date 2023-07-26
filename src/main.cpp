342181e718eb60f8954f4d0f8dae61edc29ec958
Vaaranan Yogalingam <vaaranan.y@gmail.com>
2023-07-26 14:51:17 -0400

    Initialized load scale for testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,20 @@
 #include <Arduino.h>
+#include "soc/rtc.h"
+#include "HX711.h"
+#include "esp32-hal-cpu.h"
+
+const int LOADCELL_SCK = 4;
+const int LOADCELL_DOUT = 16;
+
+HX711 scale;
 
 void setup()
 {
   Serial.begin(115200); // Set the baud rate to 115200 (change if needed)
-  delay(2000);          // Delay for 2 seconds
+  setCpuFrequencyMhz(RTC_CPU_FREQ_80M);
+  scale.begin(LOADCELL_DOUT, LOADCELL_SCK);
 
-  Serial.print("ESP32 Serial Monitor Test"); // Print a message to the serial monitor
+  Serial.print("ESP32 Serial Monitor Test\n"); // Print a message to the serial monitor
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
@@ -14,11 +23,11 @@ void loop()
   // Your main code goes here
   // Add any additional functionality or logic as needed
 
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(500);
+  // digitalWrite(LED_BUILTIN, HIGH);
+  // delay(100);
 
-  Serial.print("WAITING\n");
+  // Serial.print("WAITING\n");
 
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(1000);
+  // digitalWrite(LED_BUILTIN, LOW);
+  // delay(100);
 }
