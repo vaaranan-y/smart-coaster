e20a3efaa64f4b0871bbb782dc4003416b521934
Vaaranan Yogalingam <vaaranan.y@gmail.com>
2023-08-15 23:17:29 -0400

    fixed taring+measuring code + added scale factor

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,9 @@
 #include "HX711.h"
 #include "esp32-hal-cpu.h"
 
+#define AWS_PUB_TOPIC = "smart_coaster/pub"
+#define AWS_SUB_TOPIC = "smart_coaster/sub"
+
 const int LOADCELL_SCK = 4;
 const int LOADCELL_DOUT = 16;
 
@@ -23,19 +26,29 @@ void loop()
   // Your main code goes here
   // Add any additional functionality or logic as needed
 
+  double scaleVal = 395.32;
+
   if (scale.is_ready())
   {
     scale.set_scale();
-    Serial.println("SETTING SCALE...\n");
-    delay(3000); // time to 'zero' out the scale
+    Serial.println("Tare... remove any weights from the scale.");
+    delay(5000);
     scale.tare();
-    Serial.println("TARING COMPLETE\n");
-    Serial.println("MEASURING...\n");
-    delay(3000);                        // time to place actual weight on the scale
-    long measure = scale.get_units(10); // performs 10 readings;
-    Serial.print("Value: ");
-    Serial.println(measure);
+    Serial.println("Tare done...");
+    Serial.print("Place a known weight on the scale...");
+    delay(5000);
+    long reading = scale.get_units(10);
+    Serial.print("Result: ");
+    Serial.println(reading);
+    Serial.print("ACTUAL MEASURE: ");
+    Serial.print(reading / 395.32);
+    Serial.println("g");
   }
+  else
+  {
+    Serial.println("HX711 not found.");
+  }
+  delay(1000);
 
   // TESTING CODE
 
