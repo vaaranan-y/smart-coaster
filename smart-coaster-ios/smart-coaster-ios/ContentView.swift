7430582145a6a92716ae1c195e30effc1557c2a7
Vaaranan Yogalingam <vaaranan.y@gmail.com>
2024-11-10 21:10:02 -0500

    Added data previews (to be filled using ViewModel)

--- smart-coaster-ios/smart-coaster-ios/ContentView.swift
+++ smart-coaster-ios/smart-coaster-ios/ContentView.swift
@@ -19,8 +19,21 @@ struct ContentView: View {
             }
             .frame(maxWidth: .infinity, alignment: .topLeading)
             .padding(.top, 20)
-        Spacer()
-            
+            VStack{
+                Text("Total Water Consumed: TBA")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.top, 20)
+                    .padding(.leading, 20)
+                Text("Goal: TBA")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.top, 20)
+                    .padding(.leading, 20)
+                Text("% Complete: TBA")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.top, 20)
+                    .padding(.leading, 20)
+            }.padding(.top, 40)
+            Spacer()
         }
         .containerRelativeFrame([.horizontal, .vertical])
         .background(Gradient(colors: [.blue, .white]))
