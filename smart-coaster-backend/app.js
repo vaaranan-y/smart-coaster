b05fbabef61e95f3f9058a5801c6c043ec9c35d8
Vaaranan Yogalingam <vaaranan.y@gmail.com>
2024-11-12 21:28:34 -0500

    Basic express server config

---
+++ smart-coaster-backend/app.js
@@ -0,0 +1,6 @@
+// Application config
+
+const express = require('express');
+const app = express();
+
+app.use(express.json());
---
+++ smart-coaster-backend/server.js
@@ -0,0 +1,12 @@
+// Server config
+
+const app = require('./app');
+const port = process.env.PORT || 8080; 
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
