2c5be8e7df80fb814739d8b169f2f61d0e6d42fc
Vaaranan Yogalingam <vaaranan.y@gmail.com>
2024-11-14 22:26:18 -0500

    Gather readings from specific day

--- smart-coaster-backend/controllers/readingsController.js
+++ smart-coaster-backend/controllers/readingsController.js
@@ -15,6 +15,9 @@ const getReadings = async (req, res) => {
 // Fetch readings by TimeFrame
 const getReadingsByDate = async (req, res) => {
     const { startStamp, endStamp } = req.query; // Timestamps must be in epoch time
+    console.log(startStamp);
+    console.log(endStamp);
+
     try {
         const snapshot = await db.ref('userData/0')
                                 .orderByChild('timeStamp')
@@ -29,4 +32,38 @@ const getReadingsByDate = async (req, res) => {
     }
 };
 
-module.exports = {getReadings, getReadingsByDate}
+const getWaterConsumedOnDay = async (req, res) => {
+    const { dayStamp } = req.query;
+
+    const startOfDay = new Date(`${dayStamp}T00:00:00`);
+    const endOfDay = new Date(`${dayStamp}T23:59:59`)
+
+    const startStamp = Math.floor(startOfDay.getTime() / 1000).toString();
+    const endStamp = Math.floor(endOfDay.getTime() / 1000).toString();
+
+    console.log(startStamp)
+    console.log(endStamp)
+    // res.json({'res': 'ok'});
+
+    try {
+        const snapshot = await db.ref('userData/0')
+                                .orderByChild('timeStamp')
+                                .startAt(startStamp)
+                                .endAt(endStamp)
+                                .once('value');
+        const readings = snapshot.val();
+        console.log(readings);
+
+        for (let reading in readings) {
+            console.log(reading);
+        }
+
+        res.json(readings);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+
+
+};
+
+module.exports = {getReadings, getReadingsByDate, getWaterConsumedOnDay}
--- smart-coaster-backend/routes/readings.js
+++ smart-coaster-backend/routes/readings.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { getReadings, getReadingsByDate } = require('../controllers/readingsController');
+const { getReadings, getReadingsByDate, getWaterConsumedOnDay } = require('../controllers/readingsController');
 
 router.get('/', getReadings);
 
 // GET http://localhost:8080/api/readings/date?startStamp=1731552835&endStamp=1731552869
 router.get('/date', getReadingsByDate);
+
+// GET http://localhost:8080/api/readings/getWaterForDay?dayStamp=2024-11-13
+router.get('/getWaterForDay', getWaterConsumedOnDay);
 
 router.get('/average', (req, res) => {
     res.send('Return average reading here');
